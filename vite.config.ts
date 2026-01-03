04c2b679ddf47712eabc2a84da63b4205e84ddd9
Johannes Riedmüller <riedmueller.jo@gmail.com>
2026-01-03 18:28:23 +0100

    feat: initial vite commit no source

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ["b5e9132afce0.ngrok-free.app"],
+  },
+});
