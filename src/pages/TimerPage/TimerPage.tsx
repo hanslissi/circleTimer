e513658ab2231dd8f6f4265b0d421c6782ac400c
hanslissi <riedmueller.jo@gmail.com>
2026-08-01 10:58:55 +0200

    add new timer page

---
+++ src/pages/TimerPage/TimerPage.tsx
@@ -0,0 +1,43 @@
+import { useNavigate } from "react-router";
+import Button from "@components/Button";
+import { useTimerConfigStore } from "@state/timerConfig/useTimerConfigStore";
+import { PageLayoutWrapper } from "@layouts/PageLayoutWrapper/PageLayoutWrapper";
+import CircleTimerStepsDisplay from "@components/CircleTimerSteps/CircleTimerStepsDisplay";
+import { useStopwatchStore } from "@state/stopwatch/useStopwatchStore";
+import { ROUTE_PATHS } from "@pages/constants";
+import styles from "./TimerPage.module.css";
+
+const TimerPage = () => {
+  const navigate = useNavigate();
+  const isStopwatchRunning = useStopwatchStore((state) => state.isRunning);
+  const startStopwatch = useStopwatchStore((state) => state.start);
+  const stopStopwatch = useStopwatchStore((state) => state.stop);
+  const timerSteps = useTimerConfigStore((state) => state.steps);
+
+  const handleClickStartStop = () => {
+    if (isStopwatchRunning) {
+      stopStopwatch();
+    }
+    startStopwatch();
+  };
+
+  const handleClickEdit = () => {
+    navigate(ROUTE_PATHS.EDIT);
+  };
+
+  return (
+    <PageLayoutWrapper>
+      <div className={styles.circleTimer}>
+        <div className={styles.timerStepsSection}>
+          <CircleTimerStepsDisplay timerSteps={timerSteps} />
+        </div>
+        <div className={styles.buttonsSection}>
+          <Button onClick={handleClickEdit} className={styles.editButton}>Edit</Button>
+          <Button onClick={handleClickStartStop} className={styles.startStopButton}>{isStopwatchRunning ? "Stop" : "Start"}</Button>
+        </div>
+      </div>
+    </PageLayoutWrapper>
+  );
+};
+
+export default TimerPage;
