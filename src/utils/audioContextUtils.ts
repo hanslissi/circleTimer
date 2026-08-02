eaed533db32af7abf28b6e3295c9420033ec649b
hanslissi <riedmueller.jo@gmail.com>
2026-08-02 20:34:33 +0200

    make timeraudio unlock safer

---
+++ src/utils/audioContextUtils.ts
@@ -0,0 +1,5 @@
+export async function fetchAudioBuffer(ctx: AudioContext, url: string): Promise<AudioBuffer> {
+  const response = await fetch(url);
+  const arrayBuffer = await response.arrayBuffer();
+  return ctx.decodeAudioData(arrayBuffer);
+}
